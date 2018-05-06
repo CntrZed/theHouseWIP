2672e529af04a1df8e77f35244b3e1060e0fee3b
CntrZed <jc3d@live.com>
2018-05-06 11:41:05 -0700

    THWIPv0.001
    First Upload of base project

---
+++ theHouseWIP_v001/Assets/theHouseAssets/TemplateDesignDocs/OnStart_Disable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStart_Disable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.gameObject.SetActive(false);
+	}
+
+
+}
